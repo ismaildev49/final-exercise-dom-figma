fe14bbbaae58bd001cccebd85694d8c7e1e18d1a
ismaildev49 <ismaelbentatou@gmail.com>
2023-10-13 14:47:12 +0200

    header ok hero ok slides ok but no form in contact now button and before after the big image

---
+++ app.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 50,
+    centeredSlides: true,
+    autoplay: {
+       delay: 2500,
+      disableOnInteraction: false,
+    },
+    slidesPerView: 4,
+    loop: true,   
+  });
